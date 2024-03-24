b66990a9896585354c82224a5c15bd1146ca10be
YuriyShandor <yuriyshandor@gmail.com>
2024-03-24 19:16:56 +0200

    -wip --- add auth logic

--- auth_app/views.py
+++ auth_app/views.py
@@ -1,9 +1,23 @@
-from django.shortcuts import render
-from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 
 
 def register_page(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('blog:list')
+    else:
+        form = UserCreationForm()
     return render(request, 'auth/register.html', {'form': form})
+
+
+def login_page(request):
+    if request.method == 'POST':
+        return redirect('auth:login')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'auth/login.html', {'form': form})
