979583dc8cbc81691f0c15895b9feeef7b8b3595
YuriyShandor <yuriyshandor@gmail.com>
2024-03-24 22:07:28 +0200

    -wip --- add auth logic

--- auth_app/urls.py
+++ auth_app/urls.py
@@ -6,4 +6,5 @@ app_name = 'auth'
 urlpatterns = [
     path('register/', views.register_page, name="register"),
     path('login/', views.login_page, name="login"),
+    path('logout/', views.logout_page, name="logout"),
 ]
